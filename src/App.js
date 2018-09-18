13f42d36b4525f10d2f94c6971a84d15ddaf6e02
Violeta <violetr.23@gmail.com>
2018-09-18 12:18:46 -0400

    Burger Builder Layout Finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
+      <div>
+        <Layout>
+           <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
