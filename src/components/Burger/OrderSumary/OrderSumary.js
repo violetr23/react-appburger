597c002bf0c3e68bde4a28640b2a391fb7a2b463
Violeta <violetr.23@gmail.com>
2018-09-19 22:53:40 -0400

    Improving the App

--- src/components/Burger/OrderSumary/OrderSumary.js
+++ src/components/Burger/OrderSumary/OrderSumary.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+    /*Esta la comprobacion la hicimos para ver cuando este
+    componente se actualizaba, y por ello lo convertimos en
+    un stateFul Component, solo aclarar que puede ser un 
+    stateLess y quitamos este CycleHooks*/
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+    
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
               <li key={igKey}>
-                  <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                  <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
              </li>);
         });
-    return (
-        <Aux>
+
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to CheckOut?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    );
-};
+        );
+    }
+} 
 
-export default orderSummary;
+export default OrderSummary;
