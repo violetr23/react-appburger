d0e8e55c0a96dfbcb822ef0239f51fd8a8a9882a
Violeta <violetr.23@gmail.com>
2018-10-04 12:20:59 -0400

    Routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -30,7 +30,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
-        axios.get('https://react-my-burger-6e59e.firebaseio.com/orders/ingredients.json')
+        axios.get('https://react-my-burger-6e59e.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data});
             })
@@ -90,27 +90,17 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Violeta Rodriguez',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '41351',
-                    country: 'US'
-                },
-                email: 'test@test.com',
-            },
-            deliveryMethod: 'fastest'
+        
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({ loading: false, purchasing: false });
-        })
-        .catch(error => {
-            this.setState({ loading: false, purchasing: false });
+        queryParams.push('price=' + this.state.totalPrice)
+        const queryString = queryParams.join('&');
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
         });
     }
 
